79e0f82b35b13ef88aa729dcd801c37b6c190a07
gabrielhhaber <gabrielhaberkamp@gmail.com>
2024-09-01 15:28:22 -0300

    Primeiro commit

---
+++ buscar_cursos.php
@@ -0,0 +1,14 @@
+<?php
+
+require "vendor/autoload.php";
+use Alura\Buscador\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(["verify" => false, "base_uri" => "https://www.alura.com.br"]);
+$crawler = new Crawler();
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar("/cursos-online-programacao/php");
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
---
+++ functions.php
@@ -0,0 +1,4 @@
+<?php
+function exibeMensagem() {
+    echo "mensagem";
+}
